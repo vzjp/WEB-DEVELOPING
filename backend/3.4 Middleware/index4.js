774a45dec840eaecf24868c43e511fea2042904c
vzjp <23609056+vzjp@users.noreply.github.com>
2023-07-28 13:13:44 +0900

    change to use own middle ware for some process

--- backend/3.4 Middleware/index4.js
+++ backend/3.4 Middleware/index4.js
@@ -7,17 +7,24 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandname = "";
 
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+function bandnamemaker(req, res, next){
+  console.log(req.body);
+  bandname = req.body.street.charAt(0).toUpperCase() + req.body.street.slice(1) + req.body.pet.charAt(0).toUpperCase() + req.body.pet.slice(1);
+  next();
+}
+
+app.use(bandnamemaker);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
-  var bandname = req.body.street.charAt(0).toUpperCase() + req.body.street.slice(1)
-               + req.body.pet.charAt(0).toUpperCase() + req.body.pet.slice(1);
   res.send(`<h1>Your band name is : </h1> <p>${bandname}</p>`) 
 });
 
