033eadf0cf8348964e2dd7f8027be9351aa202ce
vzjp <23609056+vzjp@users.noreply.github.com>
2023-08-10 22:47:47 +0900

    [re-fact] remove comment which are not required

--- backend/4.4 Band Generator Project/index.js
+++ backend/4.4 Band Generator Project/index.js
@@ -6,14 +6,6 @@ const port = 3000;
 app.use(express.static("views"));
 app.use(express.static("public"));
 
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -27,13 +19,6 @@ app.post("/submit", (req, res) => {
   res.render("index.ejs", {
     band_name : band_name,
   })
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
 });
 
 app.listen(port, () => {
